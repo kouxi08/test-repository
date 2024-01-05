5d3799a0e7e8fecc58c9daaad4a007dbb2ba7f2d
kouxi08 <kouxi14@gmail.com>
2024-01-05 19:35:58 +0900

    refactor: blogとgalleryのみssrにした

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM node:18.14.1-slim as node
+FROM node:20.5.0-slim as node
 WORKDIR /usr/src/app
 COPY . /usr/src/app
 RUN npm install
-# RUN npm run build
-
+RUN npm run build
+ENV HOST=0.0.0.0
+ENV PORT=4321
+EXPOSE 4321
+CMD node ./dist/server/entry.mjs
 
 # FROM nginx:alpine AS runtime
 # COPY ./nginx/nginx.conf /etc/nginx/nginx.conf
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+// import node from '@astrojs/node';
 
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  viewTransitions: true
+  viewTransitions: true,
+  output: 'hybrid',
+  adapter: node({
+    mode: "standalone"
+  })
 });
